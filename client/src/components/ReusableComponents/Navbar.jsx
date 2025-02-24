d51fc7d639518d72cb9fc6d32f328c667c0580e5
vignesh <vigneshphr2002@gmail.com>
2025-02-24 12:56:36 +0530

    bhjbjb

--- client/src/components/ReusableComponents/Navbar.jsx
+++ client/src/components/ReusableComponents/Navbar.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, Button, IconButton } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+import { useNavigate } from 'react-router-dom';
 
 const RTONavbar = () => {
+
+    const navigate = useNavigate()
+
+    const user = JSON.parse(localStorage.getItem('user'))?.user;
+
+
+
+    const services = () => {
+
+        if (user?.role === "Officer") {
+            navigate("/officer")
+        } else if (user?.role === "Administrator") {
+            navigate("/admin")
+        } else {
+            navigate("/citizen")
+        }
+
+
+
+    }
+
     return (
-        <AppBar position="static" sx={{ backgroundColor: '#2c3e50',width:'100%' }}>
+        <AppBar position="static" sx={{ backgroundColor: '#2c3e50', width: '100%' }}>
             <Toolbar>
                 {/* <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
                     <MenuIcon />
@@ -12,8 +34,8 @@ const RTONavbar = () => {
                 <Typography variant="h6" sx={{ flexGrow: 1 }}>
                     RTO Management System
                 </Typography>
-                <Button color="inherit">Home</Button>
-                <Button color="inherit">Services</Button>
+                <Button color="inherit" onClick={() => navigate("/")}>Home</Button>
+                <Button color="inherit" onClick={services}>Services</Button>
                 <Button color="inherit">About</Button>
                 <Button color="inherit">Contact</Button>
             </Toolbar>
